b93d549b55fd6012aaa65c1a0a61e7c1c9c681a2
BIG03 <BIG03@BIG03>
2021-07-30 10:06:43 +0900

    spring boot with gradle: main page & member list

---
+++ myboot04/src/main/java/com/myboot04/member/service/MemberService.java
@@ -0,0 +1,19 @@
+package com.myboot04.member.service;
+
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+
+import com.myboot04.member.vo.MemberVO;
+
+public interface MemberService {
+	public List listMembers() throws DataAccessException;
+
+	public int addMember(MemberVO memberVO) throws DataAccessException;
+
+	public int removeMember(String id) throws DataAccessException;
+
+	public int updateMember(MemberVO memberVO) throws DataAccessException;
+
+	public MemberVO login(MemberVO memberVO) throws Exception;
+}
